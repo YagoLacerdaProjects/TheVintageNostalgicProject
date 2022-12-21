c58f2fc23761cfe4d4154d3f8405d5cbed828a80
ajeexjoji <contact@visavis-ventures.com>
2022-12-21 08:22:23 +0500

    Cosmetic feature addition
    Cosmetic feature addition of glow where touching

---
+++ Assets/Scripts/GlowSpritesManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowSpritesManager : MonoBehaviour
+{
+    public float destroyAfterSeconds;
+    public float fadetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, destroyAfterSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Color c = this.gameObject.GetComponent<SpriteRenderer>().material.color;
+
+        float current = c.a;
+
+        current = current - Time.deltaTime;
+
+        c.a = current;
+
+        this.gameObject.GetComponent<SpriteRenderer>().material.color = c;
+          
+    }
+}
