2899ab0926ac1126cc7a795dd964e1314c0cb152
ajeexjoji <contact@visavis-ventures.com>
2022-12-21 08:21:06 +0500

    Addition of main menu manager
    Added main menu manager with scenemanagement

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void loadLevel(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
